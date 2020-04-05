e5c6fa830e5f7b73e3fe2cdd94af6912b246ada8
Milan025 <58843461+Milan025@users.noreply.github.com>
2020-04-04 21:22:59 -0700

    first commit

---
+++ blog_project/blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Post, Comment
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['author','title','text','created_date','published_date']
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['post','author','text','created_date','approved_comment']
+
+admin.site.register(Post,PostAdmin)
+admin.site.register(Comment,CommentAdmin)
